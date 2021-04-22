d0d8c28539c8fb1e8ff2835c6a29ba7ba5fea046
josue_aguilar <josueaguilarjnai@gmail.com>
2021-04-22 14:00:55 -0500

    Ejercicio 3

--- tarea 5/main.c
+++ tarea 5/main.c
@@ -3,97 +3,100 @@
 //
 
 /*
-2. Escribir un programa usando switch.
-en el tendr ́an el siguiente men ́u de opciones.
-1. Ingresa tres n ́umeros, calcular su promedio.
-Si el promedio es  ́unicamente mayor a 50 mostrarlo. Si no colocar un mensaje que diga, el
-promedio es menor que 50
-2. Dados los coeficientes de la ecuaci ́on de segundo grado, determinar si las ra ́ıces son reales o
-complejas. (Nota: Verifique el valor del discriminante).
-3. Comparaci ́on de cadenas de caracteres.
-Comparar dos cadenas usando la funci ́on srtcmp y usando las variables cad1 y cad2 y regresar
-el resultado usando la variable de tipo entero llamada resultado. El valor que regresa la funci ́on
-srtcmp tiene tres opciones:
-• Si el valor retornado es cero entonces las cadenas son iguales
-• si el valor retornado es menor que cero, entonces la cad1 es menor que la cad2.
-• Si el valor retornado es mayor que cero, entonces la primer cadena (cad1 es mayor que la
-segunda cad2).
+Escribir un programa usando switch.
+Para el c ́alculo de la tasa metab ́olica basal, de mujeres y hombres. Para despu ́es calcular las
+calor ́ıas diarias recomendadas, dependiendo de la actividad realizada.
+(a) Utilizar switch para el primer menu, una vez ingresado a la opcion a o b, pedir los datos
+para el peso(en kg), altura(en cm) y a ̃nos
+(b) Realizar el calculo de la TMB (tasa metabolica basal) una vez realizado el calculo
+(c) Utilizar un switch dentro de cada uno de los dos casos anteriores(switch anidado) para tener
+un menu con cinco opciones en las que clasificaremos la actividad diaria realizada, y con
+base a eso calcular las calorias diarias recomendadas.
+(d) utilizar las siguientes ecuaciones para TMB y para las calor ́ıas diarias recomendadas.
  *
  */
 #include<stdio.h>
 #include <string.h>
 
 int main() {
-    switch (showMenu()) {
+    char opt;
+    float kg, altura, edad, tmb;
+    printf("Las ecuaciones de Harris - Benedict revisadas por Mifflin y St Jeor en 1990 ,\n"
+           "para el calculo de la tasa metabolica basal :\n"
+           "A) TMB en hombres\n"
+           "B) TMB en mujeres\n"
+           "Ingresa una opcion : ");
+    scanf("%c", &opt);
+
+    printf("\nIngresa tu peso en kg : ");
+    scanf("%i", &kg);
+    printf("\nIngresa tu altura en cm : ");
+    scanf("%i", &altura);
+    printf("\nIngresa tu edad en a%cos : ", 164);
+    scanf("%i", &edad);
+
+    switch (opt) {
+        case 'a':
+            tmb= tmbHombres(kg, altura, edad);
+            showMenuCalories(tmb);
+            break;
+        case 'b':
+            tmb = tmbMujeres(kg, altura, edad);
+            showMenuCalories(tmb);
+            break;
+        default:
+            printf("Opción erronea :c");
+            break;
+    }
+
+    return 0;
+}
+
+int showMenuCalories(float tmb){
+    int opt;
+    printf("Ingesta diaria de calorias recomendada\n"
+           "Dependiendo de la actividad diaria realizada\n"
+           "1. Poco o ningun ejercicio\n"
+           "2. Ejercicio ligero (1 -3 dias a la semana )\n"
+           "3. Ejercicio moderado (3 -5 dias a la semana )\n"
+           "4. Ejercicio fuerte (6 -7 dias a la semana )\n"
+           "5. Ejercicio muy fuerte ( dos veces al dia , entrenamientos muy duros )\n"
+           "Elige una opción: ");
+    scanf("%i", &opt);
+    switch (opt) {
         case 1:
-            promedioTresNumeros();
+            diaryCalories(tmb, 1.2);
             break;
         case 2:
-            raicesComplejas();
+            diaryCalories(tmb, 1.375);
             break;
         case 3:
-            compareStrings();
+            diaryCalories(tmb, 1.55);
+            break;
+        case 4:
+            diaryCalories(tmb, 1.725);
+            break;
+        case 5:
+            diaryCalories(tmb, 1.9);
+            break;
+        default:
+            printf("Opción invalida, nos vemos");
             break;
     }
-    return 0;
 }
-int showMenu(){
-    int opt;
-    printf("1. Ingresa tres n ́umeros, calcular su promedio."
-           "\n2. Dados los coeficientes de la ecuaci ́on de segundo grado,\n\t determinar si las ra ́ıces son reales o\n"
-           "complejas\n3. Comparaci ́on de cadenas de caracteres.\n");
-    scanf("%d", &opt);
-    return opt;
-}
-int promedioTresNumeros(){
-    float num1, num2, num3, res;
-    printf("Ingresa los 3 numeros: ");
-    scanf("%f %f %f", &num1, &num2, &num3);
-    res = (num1 + num2 + num3)/3;
-    if(res < 50){
-        printf("Promedio menor que 50");
-    }else{
-        printf("El promedio es: %.2f", res);
-    }
-    return 0;
-}
-int raicesComplejas(){
-    double a, b, c, d, sqrtA, sqrtB, sqrtC, sqrtD;
-    printf("\nIngresa los valores de a, b y c: ");
-    scanf("%d %d %d", &a, &b, &c);
-    d = b*b-4*a*c;
+int diaryCalories(float tmb, float value){
+    printf("\nTasa metabolica es : %f", tmb);
+    printf("\nCalorias diarias recomendadas : %f", tmb * value );
 
-    if (d < 0){
-        printf("\nLas raices son complejas");
-    } else{
-        printf("\nLas raices de la ecuación son reales");
-        sqrtA = sqrt(a);
-        sqrtB = sqrt(b);
-        sqrtC = sqrt(c);
-        sqrtD = sqrt(d);
-        if(sqrtA <=0){
-            printf("\na es una raiz compleja");
-        }
-        if(sqrtB <=0){
-            printf("\nb es una raiz compleja");
-        }
-        if (sqrtC <=0){
-            printf("\nc es una raiz compleja");
-        }
-        if (sqrtD <=0){
-            printf("\nd es una raiz compleja");
-        }
-    }
-    return 0;
 }
-int compareStrings(){
-    char word1[50], word2[50];
-    printf("\nIngresa 2 cadenas caracteres: ");
-    scanf("%s %s", word1, word2);
-    if(strcmp(word1, word2)==0){
-        printf("\nLas cadenas de caracteres son iguales");
-    } else {
-        printf("\nLas cadenas de caracteres son diferentes");
-    }
-    return 0;
+float tmbHombres(int kg, int altura, int edad){
+    float tmb;
+    tmb = (10 * kg) + (6.25 + altura) - (5 * edad) + 5;
+    return tmb;
+}
+
+float tmbMujeres(int kg, int altura, int edad){
+    tmb = (10 * kg) + (6.25 + altura) - (5 * edad) - 161;
+
+    return tmb;
 }
